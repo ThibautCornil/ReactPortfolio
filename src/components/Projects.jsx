1bd330f5be573b2d207dfa47993accf3c9ce633f
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2023-02-27 11:05:35 +0100

    ajout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -46,7 +46,7 @@ const Projects = () => {
                 Application React
               </span>
               <div className="pt-8 text-center">
-                <a href="https://thibautcornil.github.io/ReactCountry/">
+                <a href="https://thibautcornil.github.io/ReactCountry/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
